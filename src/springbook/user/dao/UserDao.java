35b400cf8ebbda9f6703992c40b26cfac3c7fa2b
LachesisJin <jjinimania@gmail.com>
2013-11-08 01:47:07 +0900

    add DaoFactory.java / UserDao.java / UserDaoTest.java / test-applicationContext.xml

---
+++ src/springbook/user/dao/UserDao.java
@@ -0,0 +1,96 @@
+package springbook.user.dao;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import springbook.user.domain.User;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 2SsooN
+ * Date: 2013. 11. 8.
+ * Time: AM 12:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class UserDao {
+    private DataSource dataSource;
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void add(User user) throws SQLException {
+        Connection c = this.dataSource.getConnection();
+
+        PreparedStatement ps = c.prepareStatement(
+            "INSERT INTO users(id, name, password) VALUES(?, ?, ?)");
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    public User get(String id) throws SQLException {
+        Connection c = this.dataSource.getConnection();
+
+        PreparedStatement ps = c.prepareStatement(
+            "SELECT * FROM users WHERE id = ?");
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+
+        User user = null;
+        if (rs.next()) {
+            user = new User();
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+        }
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        if (user == null) {
+            throw new EmptyResultDataAccessException(1);
+        }
+
+        return user;
+    }
+
+    public void deleteAll() throws SQLException {
+        Connection c = this.dataSource.getConnection();
+
+        PreparedStatement ps = c.prepareStatement(
+            "DELETE FROM users");
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    public int getCount() throws SQLException {
+        Connection c = this.dataSource.getConnection();
+
+        PreparedStatement ps = c.prepareStatement(
+            "SELECT COUNT(*) FROM users");
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        int count = rs.getInt(1);
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return count;
+    }
+}
