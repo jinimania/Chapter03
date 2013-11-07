74ae7945c53a68f9d66bf0e6fce3dd23849e7ded
LachesisJin <jjinimania@gmail.com>
2013-11-07 23:54:45 +0900

    add User.java

---
+++ src/springbook/user/domain/User.java
@@ -0,0 +1,47 @@
+package springbook.user.domain;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 2SsooN
+ * Date: 2013. 11. 7.
+ * Time: PM 11:53
+ * To change this template use File | Settings | File Templates.
+ */
+public class User {
+    String id;
+    String name;
+    String password;
+
+    public User() {
+    }
+
+    public User(String id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
